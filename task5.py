304248cf27d5df123a14b0ad3e381e8d6eac0c67
Vetrov-Anton <153396167+Vetrov-Anton@users.noreply.github.com>
2023-12-17 19:51:01 +0300

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,25 @@
+#x = input()
+s = ''
+w = int(input())
+step = int(input())
+with open('1full.fasta', mode='r') as f:
+    f.readline()
+    for line in f:
+        if line[0] == '>':
+            break
+        line = line.strip()
+        s = s + line
+with open('GCScew_1_out.txt', mode='w') as f:
+    cum_scew = 0
+    for i in range(0, len(s), step):
+        y = s[i:i+w]
+        if len(y) < w:
+            break
+        g = y.count('G')
+        c = y.count('C')
+        if g == 0 and c == 0:
+            scew = 0
+        else:
+            scew = (g - c) / (g + c)
+        cum_scew = cum_scew + scew
+        print(f'{i}\t{scew:.3f}\t{cum_scew:.3f}', file=f)
